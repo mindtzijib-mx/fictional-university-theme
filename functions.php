2b3452588e6002b7b0aa6cf102278fb1d9fb9790
Otilio <otiliojr1999@gmail.com>
2025-03-22 10:49:50 -0600

    Added events and programs custom post types

--- functions.php
+++ functions.php
@@ -10,3 +10,40 @@ function university_files() {
 }
 
 add_action("wp_enqueue_scripts", "university_files");
+
+function university_features() {
+    add_theme_support("title-tag");
+    register_nav_menu("exploreFooterMenu", "Explore Footer Menu");
+    register_nav_menu("learnFooterMenu", "Learn Footer Menu");
+    // add_theme_support("post-thumbnails");
+    // add_image_size("professorLandscape", 400, 260, true);
+    // add_image_size("professorPortrait", 480, 650, true);
+    // add_image_size("pageBanner", 1500, 350, true);
+}
+
+add_action("after_setup_theme", "university_features");
+
+function university_adjust_queries($query) {
+    if (!is_admin() && is_post_type_archive("program") && $query->is_main_query()) {
+        $query->set("orderby", "title");
+        $query->set("order", "ASC");
+        $query->set("posts_per_page", -1);
+    }
+
+    if (!is_admin() && is_post_type_archive("event") && $query->is_main_query()) {
+        $today = date("Ymd");
+        $query->set("meta_key", "event_date");
+        $query->set("orderby", "meta_value_num");
+        $query->set("order", "ASC");
+        $query->set("meta_query", array(
+            array(
+                "key" => "event_date",
+                "compare" => ">=",
+                "value" => $today,
+                "type" => "numeric"
+            )
+        ));
+    }
+}
+
+add_action("pre_get_posts", "university_adjust_queries");
--- header.php
+++ header.php
@@ -1,24 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
         <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
-            <a href="#"><strong>Fictional</strong> University</a>
+            <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
             </h1>
             <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
             <nav class="main-navigation">
                 <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
-                <li><a href="#">Blog</a></li>
+                <li <?php if(is_page("about-us") or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"'; ?>>
+                    <a href="<?php echo site_url("/about-us") ?>">About Us
+                    </a>
+                </li>
+                <li <?php if(get_post_type() == "program") echo 'class="current-menu-item"'; ?>>
+                    <a href="<?php echo get_post_type_archive_link("program"); ?>">
+                        Programs
+                    </a>
+                </li>
+                <li <?php if(get_post_type() == "event" || is_page("past-events")) echo 'class="current-menu-item"'; ?>>
+                    <a href="<?php echo get_post_type_archive_link("event"); ?>">
+                        Events
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Campuses
+                    </a>
+                </li>
+                <li <?php if(get_post_type() == "post") echo 'class="current-menu-item"'; ?>>
+                    <a href="<?php echo site_url("/blog") ?>">
+                    Blog
+                    </a>
+                </li>
                 </ul>
             </nav>
             <div class="site-header__util">
